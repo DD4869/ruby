5596718514ba49e9c903c82353d53f083cbc960b
DD4869 <110597693+DD4869@users.noreply.github.com>
2023-06-09 00:54:15 +0000

    Rubyを学ぼうuntil7章[メソッドを学ぼう]

---
+++ lesson6-1.rb
@@ -0,0 +1,10 @@
+puts "計算を始めます"
+puts "２つの値を入力してください"
+
+input_1 = gets.to_i #integerに変更
+input_2 = gets.to_i
+ans = input_1 * input_2
+
+puts "計算結果を出力します"
+puts "#{input_1} * #{input_2} = #{ans}"
+puts "計算を終了します"
